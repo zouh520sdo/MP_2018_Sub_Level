afba9470dc2702f7859ad59c10aed35e1e855583
zouh520sdo <zouh520sdo@gmail.com>
2018-05-25 11:28:21 -0400

    Initial work to game logic

--- Assets/Scripts/ChineseCultureCenter/Bookshelf.cs
+++ Assets/Scripts/ChineseCultureCenter/Bookshelf.cs
@@ -2,7 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct Book
+{
+    public string name;
+    public string label;
+}
+
+public class Block
+{
+    public List<Book> books;
+    public string index;
+
+    public void Put (Book book)
+    {
+        books.Add(book);
+    }
+
+    public void Remove (Book book)
+    {
+        books.Remove(book);
+    }
+
+    public bool isCorrect (Book book)
+    {
+        return book.label.Equals(index);
+    }
+}
+
 public class Bookshelf : MonoBehaviour {
+
+    public List<Block> blocks;
 
 	// Use this for initialization
 	void Start () {
